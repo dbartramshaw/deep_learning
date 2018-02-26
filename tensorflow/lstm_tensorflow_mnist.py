4e594d55b821b1b25ab922a76b0a44d70d537ed8
Bartram-Shaw, David <david.bartram-shaw@outlook.com>
2018-02-26 23:20:17 +0000

    updated utils

--- tensorflow/lstm_tensorflow_mnist.py
+++ tensorflow/lstm_tensorflow_mnist.py
@@ -109,7 +109,9 @@ def recurrent_neural_network(x):
     layer = {'weights':tf.Variable(tf.random_normal([rnn_size,n_classes])),
              'biases':tf.Variable(tf.random_normal([n_classes]))}
 
+    # Make it [n_chunks, None, chunk_size]
     x = tf.transpose(x, [1,0,2])
+    # Make it [n_chunks*n, chunk_size]
     x = tf.reshape(x, [-1, chunk_size])
     x = tf.split(x, n_chunks, 0)
 
@@ -122,7 +124,7 @@ def recurrent_neural_network(x):
 
 def train_neural_network(x):
     prediction = recurrent_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 
@@ -151,13 +153,75 @@ train_neural_network(x)
 
 
 
+####################################################
+# SIMPLE LSTM MODEL DBS
+####################################################
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.python.ops import rnn, rnn_cell
+from tensorflow.contrib import rnn
+mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+
+hm_epochs = 3
+n_classes = 10
+batch_size = 256
+time_steps = 28
+n_chunks = 28
+rnn_size = 32
 
 
+tf.reset_default_graph()
+x = tf.placeholder('float', [None, n_chunks,time_steps])
+y = tf.placeholder('float')
 
 
+def recurrent_neural_network(x):
+    layer = {'weights':tf.Variable(tf.random_normal([rnn_size,n_classes])),
+             'biases':tf.Variable(tf.random_normal([n_classes]))}
+
+    # Make it [n_chunks, None, chunk_size]
+    x = tf.transpose(x, [1,0,2])
+    # Make it [n_chunks*n, chunk_size]
+    x = tf.reshape(x, [-1, time_steps])
+    x = tf.split(x, n_chunks, 0)
+
+    # lstm_cell = rnn_cell.BasicLSTMCell(rnn_size,state_is_tuple=True)
+    # outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+
+    lstm_layer=rnn.BasicLSTMCell(rnn_size)
+    outputs,states =rnn.static_rnn(lstm_layer,x,dtype="float32")
+
+    # lstm_layer2=rnn.BasicLSTMCell(rnn_size2)
+    # outputs,states =rnn.static_rnn(lstm_layer2,outputs,dtype="float32")
+
+    output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y) )
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
 
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
 
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                epoch_x = epoch_x.reshape((batch_size,n_chunks,time_steps))
 
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
 
-#
+            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',accuracy.eval({x:mnist.test.images.reshape((-1, n_chunks, time_steps)), y:mnist.test.labels}))
+
+train_neural_network(x)
