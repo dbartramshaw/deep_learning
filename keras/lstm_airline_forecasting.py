150aeb53cab9ffa6b4b0e58a0a3b2d1b9b0995cb
Bartram-Shaw, David <david.bartram-shaw@outlook.com>
2018-04-03 23:09:49 +0100

    adding LSTM air

---
+++ keras/lstm_airline_forecasting.py
@@ -0,0 +1,427 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+	Forecasting Approaches with LSTMs
+
+    1) Simple LSTM Network for Regression: 1 feature, Y generated from X (vector)
+    2) LSTM Network for Regression - Using multilpe Features (Sliding Window )
+    3) LSTM Network for Regression - Using Multiple Time Steps
+    4) LSTM with Memory Between Batches ("Stateful" and do not suffle)
+    5) Stacked LSTMs with Memory Between Batches (return_sequences=True)
+
+"""
+
+
+
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+"""
+###############################################################################
+# LSTM Network for Regression
+###############################################################################
+# International airline passengers problem with regression framing
+# https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+
+    # international airline passengers problem with window regression framing
+
+    In this example we are inputing 1 Features
+    This is alike regression model
+    Our prediction is a single step
+
+    Our training data is generated from a single vecto
+    Using a lookback function we generate our Y
+
+    ------------------
+    X1	Y
+    112	118
+    118	132
+
+"""
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    """
+        Create LSTM format data
+        -----------------------------
+        uses X (datset) to generate Y
+        define how far you want you lookback to go
+        i.e how far do you want to predict
+    """
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+
+
+#################################
+# Data Import & Format
+#################################
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset
+dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+# LSTMs are sensitive to the scale of the input data,
+# specifically when the sigmoid (default) or tanh activation functions are used.
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# LSTM FORMAT
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+
+# reshape input to be [samples, time steps, features]
+# for Keras  [samples, time steps, features].
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX.shape
+
+
+
+#################################
+# Network build & run
+#################################
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# invert predictions
+# Must Invert so back on the same scale
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+
+#################################
+# Network Evaluate
+#################################
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot, color='r')
+plt.plot(testPredictPlot, color='g')
+plt.show()
+
+
+"""
+###############################################################################
+# LSTM Network for Regression - Sliding Window
+###############################################################################
+
+    In this example we are inputing 3 Features (the window)
+    This is alike regression model
+    Our prediction is still a single step
+
+    ------------------
+    X1	X2	X3	Y
+    112	118	132	129
+    118	132	129	121
+
+"""
+
+
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX.shape
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+# Test Score: 52.89 RMSE
+# this is eqivalent to getting it wrong by 53 people every thousand (This was the input unit)
+
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+
+
+
+"""
+###############################################################################
+# LSTM Network for Regression - Time Steps
+###############################################################################
+
+    # international airline passengers problem with window regression framing
+
+    In this example we are inputing 1 Features but using a timestpe
+    Instead of phrasing the past observations as separate input features,
+    we can use them as time steps of the one input feature,
+    which is indeed a more accurate framing of the problem.
+
+    we remember keras LSTM requires [samples, time steps, features]
+    this approach uses multiple time steps with a single Features
+
+    This shows how an LSTM can take multiple features and timesteps
+
+"""
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+""" Heres the change """
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+trainX.shape
+
+# create and fit the LSTM network
+model = Sequential()
+""" Heres the change """
+model.add(LSTM(4, input_shape=(look_back, 1)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+
+
+
+"""
+###############################################################################
+# LSTM with Memory Between Batches
+###############################################################################
+
+    The LSTM network has memory, which is capable of remembering across long sequencesself.
+    Normally, the state within the network is reset after each training batch when fitting the model,
+    as well as each call to model.predict() or model.evaluate().
+
+    "Stateful"
+    We can gain finer control over when the internal state of the LSTM network is cleared in Keras
+    by making the LSTM layer “stateful”. This means that it can build state over the entire training
+    sequence and even maintain that state if needed to make predictions.
+
+    "Do not shuffle"
+    It requires that the training data not be shuffled when fitting the network.
+    It also requires explicit resetting of the network state after each exposure to the training data
+    (epoch) by calls to model.reset_states(). This means that we must create our own outer loop of
+    epochs and within each epoch call model.fit() and model.reset_states(). For example:
+
+
+"""
+
+#LSTM for international airline passengers problem with memory
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+trainX.shape
+
+# create and fit the LSTM network
+batch_size = 1
+model = Sequential()
+""" batch_input_shape vs input_shape
+    you must :sepcify batch size
+             :stateful=True
+"""
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+for i in range(100):
+	model.fit(trainX, trainY, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
+	model.reset_states()
+
+# make predictions
+"""
+    This same batch size must then be used later when evaluating the model and making predictions.
+    Make sure you RESET AGAIN
+"""
+trainPredict = model.predict(trainX, batch_size=batch_size)
+model.reset_states()
+testPredict = model.predict(testX, batch_size=batch_size)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+
+
+
+"""
+###############################################################################
+# Stacked LSTMs with Memory Between Batches
+###############################################################################
+
+    LSTMs can be stacked alike standard MLP models
+
+    Config requirements
+    -----------------------
+    Prior LSTM layers must return a sequence (The final lasyer doesnt have to)
+    return_sequences=True
+
+#####
+"""
+
+# # Stacked LSTM for international airline passengers problem with memory
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+# create and fit the LSTM network
+batch_size = 1
+model = Sequential()
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+for i in range(100):
+	model.fit(trainX, trainY, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
+	model.reset_states()
+
+# make predictions
+trainPredict = model.predict(trainX, batch_size=batch_size)
+model.reset_states()
+testPredict = model.predict(testX, batch_size=batch_size)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
